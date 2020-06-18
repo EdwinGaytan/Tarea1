3268e86dc331d8eebd4393d5d780f5bbd087d6e0
EdwinGaytan <65833701+EdwinGaytan@users.noreply.github.com>
2020-06-18 02:43:08 -0500

    Create tarea 1_2.js

---
+++ tarea 1_2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+
+fill('red'); // Use 'redValue' in new fill
+circle(100, 100, 180); // Draw right rectangle
+
+fill(255);
+arc(100, 100, 135, 145, radians(200), radians(370), CHORD);
+  
+fill(255); // Use color variable 'c' as fill color
+arc(100, 100, 135, 135, radians(15), radians(195), CHORD);
+  
+   
+  
+  
+}
