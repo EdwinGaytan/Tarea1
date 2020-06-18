1d2e9a52e0432d6e06ae0656ca71f46fb7f7d560
EdwinGaytan <65833701+EdwinGaytan@users.noreply.github.com>
2020-06-18 02:44:37 -0500

    Create tarea 1_4.js

---
+++ tarea 1_4.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(430, 430);
+    background(0);
+  
+  fill(255,0,0);
+  rect(0,0,150,450);
+  fill(0,255,0);
+  rect(150,0,150,450);
+  fill(0,0,255);
+  rect(300,0,150,450);
+  
+}
+
+function draw(){
+}
